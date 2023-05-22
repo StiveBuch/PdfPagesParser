dac12173bfd6fb0f4f2b871e3adbc02a36705c8b
Steve <stbuchq@gmail.com>
2023-05-22 13:01:10 +0300

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include "DocumentProcessor.h"
+#include "FileUploader.h"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: program input-file\n");
+        fprintf(stderr, "\tinput-file: path of PDF, XPS, CBZ or EPUB document to open\n");
+        return EXIT_FAILURE;
+    }
+    QCoreApplication app(argc, argv);
+    DocumentProcessor docProc(argv[1]);
+    if (!docProc.processDocument()) {
+        return EXIT_FAILURE;
+    }
+
+    FileUploader uploader(docProc.getOutputFiles());
+    QObject::connect(&uploader, &FileUploader::allFilesUploaded, &app, &QCoreApplication::quit);
+    QUrl url("http://127.0.0.1:5000/upload");
+    uploader.uploadFiles(url);
+
+    return app.exec();
+}
